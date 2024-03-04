ef46b2fe19559f9febd52964b4c42cc8d7bfbff2
rajaalebchiri <rajaa.lebchiri@gmail.com>
2024-03-04 21:47:29 +0100

    adding description

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="pychoreographer",
-    version="0.0.3",
+    version="0.0.4",
+    description="A customizable Python library for automating file operations, system monitoring, and routine tasks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=["contrib", "docs", "tests*"]),
 )
